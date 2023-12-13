427a9f4d3442c1726385705100c61297f6cbe62a
VyacheslavPerebora <kulttura.hardware@gmail.com>
2023-12-13 15:42:26 +0300

    add @pytest.mark.xfail to the failed tests in test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 import pytest
 from .pages.product_page import ProductPage
 
-'''
+
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -22,7 +22,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_message_with_basket_cost_and_match_prices()
 
 
-
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -39,13 +39,14 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message_el_must_not_appeared()
 
 
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.add_item_to_basket()
     page.should_not_be_success_message_el_must_disappeared()
-'''
+
 
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
