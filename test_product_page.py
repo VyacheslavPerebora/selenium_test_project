32a433405876ad6e77cd8d26aa9a2ccdc5dd35f8
VyacheslavPerebora <kulttura.hardware@gmail.com>
2023-12-14 21:48:45 +0300

    remove quotes in tets_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@ import pytest
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 import time
-'''
+
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -61,7 +61,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
-'''
+
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
