8e539d1b5e4a49c14a70b30762ddd12c65d6082f
VyacheslavPerebora <kulttura.hardware@gmail.com>
2023-12-15 15:12:51 +0300

    we combine two tests into one class in the file test_main_page.py and mark it with the label 'login_guest' when running, add -m login_guest

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,10 +8,10 @@ import math
 
 class BasePage:
 
-    def __init__(self, browser, url):  #, timeout=10):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        #self.browser.implicitly_wait(timeout)
+        self.browser.implicitly_wait(timeout)
 
     def go_to_login_page(self):
         login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
